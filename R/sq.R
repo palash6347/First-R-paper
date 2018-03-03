2533a32f46874a8f6ce7981140afac1ed26a9af3
palash6347 <psharma4@kumc.edu>
2018-03-03 17:30:30 -0600

    My first R package

---
+++ R/sq.R
@@ -0,0 +1,12 @@
+#' It use to square a value
+#'
+#' @param x A numeric value is calculated
+#'
+#' @return The square of the input
+#' @export
+#'
+#' @examples square(10)
+square<-function(x){
+  return(x^2)
+}
+
